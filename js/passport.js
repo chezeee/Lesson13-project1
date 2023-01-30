81770d62979ca6c8dbc67e458058bcc2b0bf1a2c
Ilya Yalymov <chezeee@gmail.com>
2023-01-30 20:43:51 +0300

    Problem solution added

--- js/passport.js
+++ js/passport.js
@@ -3,5 +3,33 @@ export default class Passport {
    * @param {string} firstName
    * @param {string} lastName
    */
-  // your code
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFirstName() {
+    return this.firstName.toLowerCase();
+  }
+
+  getLastName() {
+    return this.lastName.toUpperCase();
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getInitials() {
+    return `${this.firstName[0].toUpperCase()}.${this.lastName[0].toUpperCase()}.`;
+  }
+
+  getIsValidName() {
+    const pattern = /^[a-zA-Z]{1,}$/;
+    return pattern.test(this.firstName) &&
+      pattern.test(this.lastName) &&
+      !this.lastName.endsWith(".")
+      ? "Yes"
+      : "No";
+  }
 }
